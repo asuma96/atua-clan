2aad1b59c11e3a4475352c334f5e2429b01e6d12
asuma96 <shichkov1996@gmail.com>
2023-03-22 21:17:09 +0300

    local create seeds and fix session

--- database/seeders/AnswerSeeder.php
+++ database/seeders/AnswerSeeder.php
@@ -18,64 +18,213 @@ class AnswerSeeder extends Seeder
         $answers = [
             [
                 'question_id' => '1',
+                'answer' => 'HATIN',
+                'right_answer' => '1',
+            ],
+            [
+                'question_id' => '1',
+                'answer' => 'Impulse',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '1',
+                'answer' => 'Sparks',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '1',
+                'answer' => 'Superpowers',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '2',
+                'answer' => 'I NEVA GO HARD',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '2',
+                'answer' => 'SENJU',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '2',
+                'answer' => 'MIRROR TALK',
+                'right_answer' => '1',
+            ],
+            [
+                'question_id' => '2',
+                'answer' => 'POKE',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '3',
+                'answer' => 'LAST ONE',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '3',
+                'answer' => 'I NEVA GO HARD',
+                'right_answer' => '1',
+            ],
+            [
+                'question_id' => '3',
+                'answer' => 'BORED',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '3',
+                'answer' => 'WICKED',
+                'right_answer' => '0',
+            ],
+
+            [
+            'question_id' => '4',
+            'answer' => 'OOLONG',
+            'right_answer' => '1',
+            ],
+            [
+                'question_id' => '4',
+                'answer' => 'COMSI',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '4',
+                'answer' => 'SET IT FREE',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '4',
+                'answer' => 'THUNDAWIND',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '5',
+                'answer' => 'NOX',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '5',
+                'answer' => 'SENJU',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '5',
+                'answer' => 'SLIPPERS',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '5',
+                'answer' => 'POKE',
+                'right_answer' => '1',
+            ],
+
+            [
+                'question_id' => '6',
                 'answer' => 'TTM',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '6',
+                'answer' => 'QUALITY',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '6',
+                'answer' => 'ITAMI',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '6',
+                'answer' => 'ROOKIE',
+                'right_answer' => '1',
+            ],
+
+            [
+                'question_id' => '7',
+                'answer' => 'NO LETTERS',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '7',
+                'answer' => 'IDU',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '7',
+                'answer' => 'SENJU',
                 'right_answer' => '1',
             ],
             [
-                'question_id' => '1',
-                'answer' => 'Thundra',
+                'question_id' => '7',
+                'answer' => 'THOMPSON',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '8',
+                'answer' => 'RAID',
                 'right_answer' => '0',
             ],
             [
-                'question_id' => '1',
-                'answer' => 'I neva go hard',
-                'right_answer' => '0',
-            ],
-            [
-                'question_id' => '1',
-                'answer' => 'Пустошь',
-                'right_answer' => '0',
-            ],
-            [
-                'question_id' => '2',
-                'answer' => 'aggregator-create',
+                'question_id' => '8',
+                'answer' => 'THUNDAWIND',
                 'right_answer' => '1',
             ],
             [
-                'question_id' => '2',
-                'answer' => 'aggregator-create',
+                'question_id' => '8',
+                'answer' => 'ENERGY',
                 'right_answer' => '0',
             ],
             [
-                'question_id' => '2',
-                'answer' => 'Metal birds',
+                'question_id' => '8',
+                'answer' => 'OSAKA CRY',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '9',
+                'answer' => 'TORII',
                 'right_answer' => '0',
             ],
             [
-                'question_id' => '2',
-                'answer' => 'tha-namez',
-                'right_answer' => '0',
-            ],
-            [
-                'question_id' => '3',
-                'answer' => 'superpowers',
+                'question_id' => '9',
+                'answer' => 'TWOX',
                 'right_answer' => '1',
             ],
             [
-                'question_id' => '3',
-                'answer' => 'lazy boy',
+                'question_id' => '9',
+                'answer' => 'NOMAD',
                 'right_answer' => '0',
             ],
             [
-                'question_id' => '3',
-                'answer' => 'vrubel',
+                'question_id' => '9',
+                'answer' => 'OBLIVION',
+                'right_answer' => '0',
+            ],
+
+            [
+                'question_id' => '10',
+                'answer' => 'VRUBEL',
                 'right_answer' => '0',
             ],
             [
-                'question_id' => '3',
-                'answer' => 'no letters',
+                'question_id' => '10',
+                'answer' => 'CRUISIN',
                 'right_answer' => '0',
-            ]
+            ],
+            [
+                'question_id' => '10',
+                'answer' => 'I GET DOWN',
+                'right_answer' => '0',
+            ],
+            [
+                'question_id' => '10',
+                'answer' => 'UNCLE DECOY',
+                'right_answer' => '1',
+            ],
         ];
 
         foreach ($answers as $answer) {
